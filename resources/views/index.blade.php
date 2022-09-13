a175e46bd5b117ba2474995c14a7c342d3e505b1
Abdullah Zahra <abdullahmzahra@gmail.com>
2022-09-13 21:16:51 +0200

    views

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welocme To Auth2.0</title>
+</head>
+<body>
+    <h1>Welcome to Auth2.0</h1>
+    <a href="{{ url('/register') }}">Register</a>
+</body>
+</html>
